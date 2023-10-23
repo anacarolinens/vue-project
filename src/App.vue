51a4ec4e1ba542fc68eb3d1d911426a7d2f5d7b2
Ana Caroline <85416744+anacarolinens@users.noreply.github.com>
2023-10-23 17:02:06 -0500

    Updates componentes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup> </script>
+<script setup> 
+import Cabecalho from './components/Cabecalho.vue';
+import NavbarVue from './components/Navbar.vue';
+</script>
 
 <template>
   <header>
-   
+    <Nav></Nav>
+    <Cabecalho></Cabecalho>
   </header>
 
   <main>
@@ -10,7 +14,14 @@
   </main>
 </template>
 
-<style scoped>
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+
+}
 
 header {
   line-height: 1.5;
---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,17 @@
+<script setup>
+const nome = 'Home'
+</script>
+<template>
+    <div>
+        <h1 class="title">{{ nome }}</h1>
+    </div>
+</template>
+
+ 
+<style scoped>
+.title {
+    max-width: 0 auto;
+    text-align: center;
+    color: red;
+}
+</style>
